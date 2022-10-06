0ea2bb34f5a430fa4b8c61d49790e0cdc68b30db
Jordan Butterfield <jmcbutter@gmail.com>
2022-10-06 06:31:49 -0500

    Begin refactoring JS

--- src/script.js
+++ src/script.js
@@ -1,3 +1,6 @@
+let themeSwitch = document.getElementById("toggle");
+themeSwitch.addEventListener("change", (e) => document.body.setAttribute("data-theme", e.target.value));
+
 const VALID_KEYS = [
   "1",
   "2",
@@ -19,251 +22,237 @@ const display = document.getElementById("display");
 const heldDisplay = document.getElementById("held-calc");
 const buttons = document.querySelectorAll("[data-key]");
 
-let displayValue;
+//TODO: Limit size of answer, provide eX when answer exceeds bounds of display using toExponential()
+//TODO: Limit size of heldDisplay, provide eX when answer exceeds bounds
+//TODO: Limit size of decimals to 10 places(?)
+
 let heldCalculationValue = 0;
 let heldCalculationOperator = "+";
+let state = State();
+
+let operators = document.querySelectorAll("[data-operator]");
+operators.forEach(operator => {
+  operator.addEventListener("click", (e) => {
+    let key = e.target.getAttribute("data-operator");
+    let displayedValue = display.value;
+    state.operateOnState(displayedValue);
+    state.setOperator(key);
+  })
+})
 
 buttons.forEach((button) => {
   button.addEventListener("click", (e) => {
+    let key = e.target.getAttribute("data-key");
+    let displayedValue = display.value;
     switch (e.target.getAttribute("data-key")) {
-      case "0":
-        if (heldCalculationOperator == "=") {
-          heldCalculationValue = "0";
-          heldCalculationOperator = "+";
-          display.value = 0;
-        }
-        if (display.value == "0") {
-          display.value = "0";
-        } else {
-          display.value += "0";
-        }
-        break;
-      case "1":
-        if (heldCalculationOperator == "=") {
-          heldCalculationValue = "0";
-          heldCalculationOperator = "+";
-          display.value = 0;
-        }
-        if (display.value == "0") {
-          display.value = "1";
-        } else {
-          display.value += "1";
-        }
-        break;
-      case "2":
-        if (heldCalculationOperator == "=") {
-          heldCalculationValue = "0";
-          heldCalculationOperator = "+";
-          display.value = 0;
-        }
-        if (display.value == "0") {
-          display.value = "2";
-        } else {
-          display.value += "2";
-        }
-        break;
-      case "3":
-        if (heldCalculationOperator == "=") {
-          heldCalculationValue = "0";
-          heldCalculationOperator = "+";
-          display.value = 0;
-        }
-        if (display.value == "0") {
-          display.value = "3";
-        } else {
-          display.value += "3";
-        }
-        break;
-      case "4":
-        if (heldCalculationOperator == "=") {
-          heldCalculationValue = "0";
-          heldCalculationOperator = "+";
-          display.value = 0;
-        }
-        if (display.value == "0") {
-          display.value = "4";
-        } else {
-          display.value += "4";
-        }
-        break;
-      case "5":
-        if (heldCalculationOperator == "=") {
-          heldCalculationValue = "0";
-          heldCalculationOperator = "+";
-          display.value = 0;
-        }
-        if (display.value == "0") {
-          display.value = "5";
-        } else {
-          display.value += "5";
-        }
-        break;
-      case "6":
-        if (heldCalculationOperator == "=") {
-          heldCalculationValue = "0";
-          heldCalculationOperator = "+";
-          display.value = 0;
-        }
-        if (display.value == "0") {
-          display.value = "6";
-        } else {
-          display.value += "6";
-        }
-        break;
-      case "7":
-        if (heldCalculationOperator == "=") {
-          heldCalculationValue = "0";
-          heldCalculationOperator = "+";
-          display.value = 0;
-        }
-        if (display.value == "0") {
-          display.value = "7";
-        } else {
-          display.value += "7";
-        }
-        break;
-      case "8":
-        if (heldCalculationOperator == "=") {
-          heldCalculationValue = "0";
-          heldCalculationOperator = "+";
-          display.value = 0;
-        }
-        if (display.value == "0") {
-          display.value = "8";
-        } else {
-          display.value += "8";
-        }
-        break;
-      case "9":
-        if (heldCalculationOperator == "=") {
-          heldCalculationValue = "0";
-          heldCalculationOperator = "+";
-          display.value = 0;
-        }
-        if (display.value == "0") {
-          display.value = "9";
-        } else {
-          display.value += "9";
-        }
-        break;
-      case ".":
-        if (heldCalculationOperator == "=") {
-          heldCalculationValue = "0";
-          heldCalculationOperator = "+";
-          display.value = 0;
-        }
-        if (display.value.includes(".")) break;
-        if (display.value == "0") {
-          display.value = "0.";
-        } else {
-          display.value += ".";
-        }
-        break;
       case "+":
-        switch (heldCalculationOperator) {
-          case "+":
-            heldCalculationValue += +display.value;
-            break;
-          case "-":
-            heldCalculationValue -= +display.value;
-            break;
-          case "*":
-            heldCalculationValue *= +display.value;
-            break;
-          case "/":
-            heldCalculationValue /= +display.value;
-            break;
-        }
-        heldCalculationOperator = "+";
-        heldDisplay.textContent = +heldCalculationValue + " + ";
-        display.value = 0;
-        break;
       case "-":
-        switch (heldCalculationOperator) {
-          case "+":
-            heldCalculationValue += +display.value;
-            break;
-          case "-":
-            heldCalculationValue -= +display.value;
-            break;
-          case "*":
-            heldCalculationValue *= +display.value;
-            break;
-          case "/":
-            heldCalculationValue /= +display.value;
-            break;
-        }
-        heldCalculationOperator = "-";
-        heldDisplay.textContent = +heldCalculationValue + " - ";
-        display.value = 0;
-        break;
       case "x X *":
-        switch (heldCalculationOperator) {
-          case "+":
-            heldCalculationValue += +display.value;
-            break;
-          case "-":
-            heldCalculationValue -= +display.value;
-            break;
-          case "*":
-            heldCalculationValue *= +display.value;
-            break;
-          case "/":
-            heldCalculationValue /= +display.value;
-            break;
-        }
-        heldCalculationOperator = "*";
-        heldDisplay.textContent = +heldCalculationValue + " x ";
-        display.value = 0;
-        break;
       case "/":
-        switch (heldCalculationOperator) {
-          case "+":
-            heldCalculationValue += +display.value;
-            break;
-          case "-":
-            heldCalculationValue -= +display.value;
-            break;
-          case "*":
-            heldCalculationValue *= +display.value;
-            break;
-          case "/":
-            heldCalculationValue /= +display.value;
-            break;
-        }
-        heldCalculationOperator = "/";
-        heldDisplay.textContent = +heldCalculationValue + " / ";
-        display.value = 0;
-        break;
       case "Enter =":
         switch (heldCalculationOperator) {
           case "+":
-            displayValue = +heldCalculationValue + +display.value;
+            heldCalculationValue += +displayedValue;
             break;
           case "-":
-            displayValue = +heldCalculationValue - +display.value;
+            heldCalculationValue -= +displayedValue;
             break;
-          case "*":
-            displayValue = +heldCalculationValue * +display.value;
+          case "x":
+            heldCalculationValue *= +displayedValue;
             break;
           case "/":
-            displayValue = +heldCalculationValue / +display.value;
+            heldCalculationValue /= +displayedValue;
             break;
         }
-        heldCalculationOperator = "=";
-        heldCalculationValue = displayValue;
-        heldDisplay.textContent = "";
-        display.value = +displayValue;
+        heldCalculationOperator = operator(key);
+        heldDisplay.textContent = heldDisplayValue(key);
+        display.value = displayValue(key);
         break;
       case "Delete Backspace":
-        display.value = display.value.slice(0, -1);
-        if (display.value == 0) display.value = "0";
+        if (heldCalculationOperator == "=") {
+         heldCalculationValue = "0";
+         heldCalculationOperator = "+"; 
+        }
+        if (display.value == "") {
+          display.value = "0";
+        } else {
+          display.value = displayedValue.slice(0, -1);
+        }
         break;
       case "C c":
-        heldCalculationOperator = "+";
-        heldCalculationValue = "0";
-        heldDisplay.textContent = "";
         display.value = "0";
+        
+        heldCalculationValue = "0";
+        heldCalculationOperator = "+";
+        
+        heldDisplay.textContent = "";
+        break;
+      case ".":
+        if (display.value.includes(".")) break;
+      default:
+        if (heldCalculationOperator == "=") {
+          display.value = "0";
+         
+          heldCalculationValue = "0";
+          heldCalculationOperator = "+";
+        }
+        if (display.value == "0") {
+          display.value = newNumeric(key);
+        } else {
+          display.value += key;
+        }
+        break;
+    }
+
+    function newNumeric(key) {
+      if (key == ".") {
+        return "0" + key;
+      } else {
+        return key;
+      }
+    }
+
+    function operator(key) {
+      if (key == "x X *") {
+        return "x";
+      } else if (key == "Enter =") {
+        return "=";
+      } else {
+        return key;
+      }
+    }
+
+    function heldDisplayValue(key) {
+      if (key == "Enter =") {
+        return "";
+      } else {
+        return +heldCalculationValue + " " + operator(key) + " ";
+      }
+    }
+
+    function displayValue(key) {
+      if (key == "Enter =") {
+        return +heldCalculationValue;
+      } else {
+        return "0";
+      }
     }
   });
 });
+
+function State() {
+  let value = "0";
+  let operator = "+";
+
+  function operateOnState(newValue) {
+    newValue = +newValue;
+    switch (operator) {
+      case "+":
+        value += newValue;
+        break;
+      case "-":
+        value -= newValue;
+        break;
+      case "x":
+        value *= newValue;
+        break;
+      case "/":
+        value /= newValue;
+        break;
+    }
+  }
+
+  function setOperator(newOperator) {
+    operator = newOperator;
+    console.log(operator);
+  }
+
+  function setValue(newValue) {
+    value = newValue;
+  }
+
+  function getValue() {
+    return value;
+  }
+  
+  return {
+    operateOnState,
+    setOperator,
+    setValue,
+    getValue,
+  }
+}
+  // let displayedValue = display.value;
+  //     heldCalculationOperator = operator(key);
+  //     heldDisplay.textContent = heldDisplayValue(key);
+  //     display.value = displayValue(key);
+  //     break;
+  //   case "Delete Backspace":
+  //     if (heldCalculationOperator == "=") {
+  //       heldCalculationValue = "0";
+  //       heldCalculationOperator = "+"; 
+  //     }
+  //     if (display.value == "") {
+  //       display.value = "0";
+  //     } else {
+  //       display.value = displayedValue.slice(0, -1);
+  //     }
+  //     break;
+  //   case "C c":
+  //     display.value = "0";
+      
+  //     heldCalculationValue = "0";
+  //     heldCalculationOperator = "+";
+      
+  //     heldDisplay.textContent = "";
+  //     break;
+  //   case ".":
+  //     if (display.value.includes(".")) break;
+  //   default:
+  //     if (heldCalculationOperator == "=") {
+  //       display.value = "0";
+        
+  //       heldCalculationValue = "0";
+  //       heldCalculationOperator = "+";
+  //     }
+  //     if (display.value == "0") {
+  //       display.value = newNumeric(key);
+  //     } else {
+  //       display.value += key;
+  //     }
+  //     break;
+  // }
+
+  // function newNumeric(key) {
+  //   if (key == ".") {
+  //     return "0" + key;
+  //   } else {
+  //     return key;
+  //   }
+  // }
+
+  // function operator(key) {
+  //   if (key == "x X *") {
+  //     return "x";
+  //   } else if (key == "Enter =") {
+  //     return "=";
+  //   } else {
+  //     return key;
+  //   }
+  // }
+
+  // function heldDisplayValue(key) {
+  //   if (key == "Enter =") {
+  //     return "";
+  //   } else {
+  //     return +heldCalculationValue + " " + operator(key) + " ";
+  //   }
+  // }
+
+  // function displayValue(key) {
+  //   if (key == "Enter =") {
+  //     return +heldCalculationValue;
+  //   } else {
+  //     return "0";
+  //   }
+  // }
